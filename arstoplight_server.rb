5cab14b03972332a5c21e313371bc86c6f512f22
Kevin Mullin <kevin@kpmullin.com>
2011-09-19 21:08:56 -0700

    updated for testing

--- arstoplight_server.rb
+++ arstoplight_server.rb
@@ -6,16 +6,23 @@ end
 
 PORT = 22222
 RESPONSES = ['1', '2', '3', 'F']
+SERIAL_PORT, SPEED = '/dev/tty.usbmodemfd121', 9600
 
 def check_responses(data)
   if RESPONSES.include?(data)
+    write_serial data
     return data
   end
 end
 
-server = TCPServer.open(PORT.to_i)
+def write_serial(data)
+  @serial.write(data)
+end
+
+@serial = SerialPort.new(SERIAL_PORT,SPEED)
+@server = TCPServer.open(PORT.to_i)
 loop {
-  client = server.accept
+  client = @server.accept
   stuff = check_responses(client.read(1))
   client.puts stuff unless stuff.nil?
   client.close
