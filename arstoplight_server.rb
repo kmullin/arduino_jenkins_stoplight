9cc70cd1d2e3195c971c6ca50d068a9d8e51602a
Kevin Mullin <kevin@kpmullin.com>
2011-09-20 10:26:27 -0700

    touched up a bit, working with json

--- arstoplight_server.rb
+++ arstoplight_server.rb
@@ -4,38 +4,54 @@
   require m
 end
 
-test_json = '{
-  "name":"JobName",
-  "url":"JobUrl",
-  "build":{
-    "number":1,
-    "phase":"STARTED",
-    "status":"FAILED",
-    "url":"job/project/5",
-    "fullUrl":"http://ci.jenkins.org/job/project/5"
-  }
-}'
-
+DEBUG = true
+PROJECT = 'testing'
 PORT = 22222
 RESPONSES = ['1', '2', '3', 'F']
 SERIAL_PORT, SPEED = '/dev/tty.usbmodemfd121', 9600
 
-def check_responses(data)
-  if RESPONSES.include?(data)
-    write_serial data
-    return data
+def check_response(json_hash)
+  if json_hash["name"] == PROJECT
+    if json_hash["build"]["phase"] == 'FINISHED'
+      if json_hash["build"]["status"] == 'SUCCESS'
+        write_serial('3')
+      elsif json_hash["build"]["status"] == 'FAILURE'
+        write_serial('1')
+      elsif json_hash["build"]["status"] == 'ABORTED'
+        write_serial('F')
+      else
+        write_serial('2')
+      end
+    end
   end
 end
 
 def write_serial(data)
-  @serial.write(data)
+  if RESPONSES.include?(data)
+    puts "Serial Sending '#{data}'" if DEBUG
+    @serial.write(data)
+  end
 end
 
-@serial = SerialPort.new(SERIAL_PORT,SPEED)
+begin
+  @serial = SerialPort.new(SERIAL_PORT,SPEED)
+rescue Errno::ENOENT
+  puts "Can't find #{SERIAL_PORT}"
+  exit(1)
+end
 @server = TCPServer.open(PORT.to_i)
+
+count = 0
 loop {
   client = @server.accept
-  stuff = check_responses(client.read(1))
-  client.puts stuff unless stuff.nil?
-  client.close
+  count += 1
+  puts "Client \##{count}" if DEBUG
+  begin
+    hash = Yajl::Parser.parse(client.read(512))
+    client.close
+    check_response(hash)
+  rescue Yajl::ParseError
+    puts "Client \##{count} - Error parsing" if DEBUG
+    next
+  end
 }
